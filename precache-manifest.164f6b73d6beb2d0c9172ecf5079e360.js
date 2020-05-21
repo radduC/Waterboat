177b0a077d690de21c8e65a157c7b6abe8e18f37
radu costache <sir.raddu@gmail.com>
2020-05-21 10:18:40 +0300

    first commit

---
+++ precache-manifest.164f6b73d6beb2d0c9172ecf5079e360.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089448e78ee881887f05774255f4c2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33eabc3e9e4a288dfe69",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "7d06cbb111a3081b3fb2",
+    "url": "/static/css/main.eadb9a8c.chunk.css"
+  },
+  {
+    "revision": "33eabc3e9e4a288dfe69",
+    "url": "/static/js/2.fbf5aac4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fbf5aac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d06cbb111a3081b3fb2",
+    "url": "/static/js/main.648018b3.chunk.js"
+  },
+  {
+    "revision": "a40d8d96c5ef1b8f5796",
+    "url": "/static/js/runtime-main.3e6c43a3.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
